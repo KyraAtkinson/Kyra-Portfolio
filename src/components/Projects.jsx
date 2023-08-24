530b484f179b72f35c47bfb062978a237b668e56
kyra <mykalbusiness0112@gmail.com>
2023-08-24 13:35:18 -0400

    final commit part two

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -79,7 +79,7 @@ const Projects = () => {
                     Demo
                   </button>
                 </a>
-                <a href=' /'>
+                <a href='https://github.com/KyraAtkinson/Kyra-Portfolio'>
                   <button className='text-center rounded-lg px-4 py-3  m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
